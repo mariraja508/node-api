f56602a42cacf28d4a792de2c6299e7372c8773e
mariraja508 <82996325+mariraja508@users.noreply.github.com>
2021-05-20 14:21:33 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -9,27 +9,6 @@ const app = express(); // return instance of the app
 // Setting up the cors config
 app.use(cors());
 
-// Tell the app to parse the body of the request
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-// Setting up the dotenv config
-dotenv.config({
-  path: './.env'
-});
-
-// Setup the routing
-fs.readdirSync(`${__dirname}/routes`).map((file) => {
-  require(`./routes/${file}`)(app);
-});
-
-app.listen(process.env.PORT, (err) => {
-  if (err) {
-    console.log(err); // TODO: improve the error handling
-    process.exit(1);
-  }
-
-  require('./utils/db');
-
-  console.log(`CORS-enabled web server is now running in ${process.env.NODE_ENV} on port ${process.env.PORT}`);
-});
+app.get('/',function(req,res){
+  res.send({'name' : 'Mariraja'})
+})
